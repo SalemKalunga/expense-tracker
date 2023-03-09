af2c2fcc8efb02e929f1976735c235df8a99db42
Salem Kalunga <salemlegrand65@gmail.com>
2023-03-09 17:45:36 +0200

    little optimization on header

--- src/components/header.component/header.component.jsx
+++ src/components/header.component/header.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./header.style";
-import { ButtonContainer, StyledHeader } from "./header.style";
+import { StyledHeader } from "./header.style";
 import { Outlet } from "react-router-dom";
 import { signUserOut } from "../../utils/firebase";
 
