467aa155e32c2e8fe558ff1e723f98345cbddc5d
Salem Kalunga <salemlegrand65@gmail.com>
2023-03-09 17:39:26 +0200

    update the header

--- src/components/header.component/header.component.jsx
+++ src/components/header.component/header.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./header.style";
-import { StyledHeader } from "./header.style";
+import { ButtonContainer, StyledHeader } from "./header.style";
 import { Outlet } from "react-router-dom";
 import { signUserOut } from "../../utils/firebase";
 
@@ -8,7 +8,11 @@ const Header = () => {
   return (
     <>
       <StyledHeader>
-        <h1>Exp</h1>
+        <img
+          src="https://abscongo.com/congo/assets/images/icons/some/logo6.png"
+          width={60}
+          alt="logo"
+        />
         <button onClick={signUserOut}>SignOut</button>
       </StyledHeader>
       <Outlet />
--- src/components/header.component/header.style.jsx
+++ src/components/header.component/header.style.jsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
-import { responsivePadding } from "../dashboard/dashboard.style";
+import { flexBox, responsivePadding } from "../dashboard/dashboard.style";
 export const StyledHeader = styled.header`
   box-shadow: 0px 3px 3px lightgray;
   ${responsivePadding}
+  ${flexBox("row", "space-between", "space-between", 1)};
+  button {
+    padding: 0.2rem 1rem;
+    display: inline-block;
+  }
 `;
